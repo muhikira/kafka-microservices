81a725de704e4d7c599de03d332b2f290bb315a9
Muhikira <muhikiraismael@gmail.com>
2023-08-20 00:54:23 -0500

    first commit

--- src/main/java/com/muhikira/basedomains/dto/OrderEvent.java
+++ src/main/java/com/muhikira/basedomains/dto/OrderEvent.java
@@ -1,2 +1,14 @@
-package com.muhikira.basedomains.dto;public class OrderEvent {
+package com.muhikira.basedomains.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderEvent {
+    private String message;
+    private String status;
+    private Order order;
 }
